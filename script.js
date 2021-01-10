2793d9d5b39ad17fa1afcf780995f4c2d71d3e8f
Nick Mardon <nickmardondigital@gmail.com>
2021-01-10 14:26:46 -0800

    pause play stop working

---
+++ script.js
@@ -0,0 +1,51 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play and Pause Video
+function toggleVideoStatus() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// update play pause icon
+function updatePlayIcon() {
+    if(video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+// update progress and timestamp
+function updateProgress() {
+    return true;
+}
+
+//set video progress
+function setVideoProgess() {
+    return true;
+}
+
+// Event Listeners
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgess);
